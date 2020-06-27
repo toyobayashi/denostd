d1588932e788e822bbd71af62e4b3294166117fd
toyobayashi <lifenglin314@outlook.com>
2020-06-27 23:37:15 +0800

    use got

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -1,40 +1,31 @@
-const fetch = require('node-fetch').default
 const path = require('path')
 /** @type {typeof import('fs-extra')} */
 const fs = require('fs-extra')
-
-const { unzipSync } = require('@tybys/cross-zip')
+const got = require('got').default
 
 ;(async function main (args) {
   const version = args[0]
   if (version === undefined) {
     throw new Error('Version required.')
   }
-  
-  const res = await fetch(`https://github.com/denoland/deno/archive/std/${version}.zip`, {
-    method: 'GET',
+
+  const stream = got.stream(`https://github.com/denoland/deno/archive/std/${version}.zip`, {
     headers: {
       'User-Agent': 'denostd'
     }
   })
 
-  let size = -1
-  if (res.headers.has('content-length')) {
-    size = Number(res.headers.has('content-length'))
-  }
-
-  let l = 0
-  res.body.on('data', (chunk) => {
-    l += chunk.length
-    if (size !== -1) {
-      const percent = l / size
-      process.stdout.write(`\x1b[666D\x1b[0KDownload ${version}.zip: ${l} / ${size} ${(Math.floor(percent * 10000) / 100).toFixed(2)}%`)
+  stream.on('downloadProgress', (progress) => {
+    const l = progress.transferred
+    const percent = progress.percent
+    if (progress.total != null) {
+      process.stdout.write(`\x1b[666D\x1b[0KDownload ${version}.zip: ${l} / ${progress.total} ${(Math.floor(percent * 10000) / 100).toFixed(2)}%`)
     } else {
       process.stdout.write(`\x1b[666D\x1b[0KDownload ${version}.zip: ${l} / Unknown`)
     }
   })
 
-  res.body.on('error', (err) => {
+  stream.on('error', (err) => {
     console.error(err)
     process.exit(1)
   })
@@ -51,14 +42,14 @@ const { unzipSync } = require('@tybys/cross-zip')
   dest.on('close', () => {
     console.log('\nDone.')
     fs.renameSync(target + '.tmp', target)
-    unzipSync(target, join(__dirname, `../temp/${version}`))
-    fs.removeSync(join(__dirname, `../cli/js`))
-    fs.copySync(join(__dirname, `../temp/${version}/cli/js`), join(__dirname, `../cli/js`))
-    fs.removeSync(join(__dirname, `../std`))
-    fs.copySync(join(__dirname, `../temp/${version}/std`), join(__dirname, `../std`))
+    unzipSync(target, path.join(__dirname, `../temp/${version}`))
+    fs.removeSync(path.join(__dirname, `../cli/js`))
+    fs.copySync(path.join(__dirname, `../temp/${version}/cli/js`), path.join(__dirname, `../cli/js`))
+    fs.removeSync(path.join(__dirname, `../std`))
+    fs.copySync(path.join(__dirname, `../temp/${version}/std`), path.join(__dirname, `../std`))
     fs.removeSync(tmpDir)
   })
-  res.body.pipe(dest)
+  stream.pipe(dest)
 })(process.argv.slice(2)).catch(err => {
   console.error(err)
   process.exit(1)
